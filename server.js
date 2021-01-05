35a6c18be662c2cab75df2fb7951598bcb937858
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-05 14:27:42 -0500

    Add comments

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import apiRoutes from "./routes/api-routes.js";
 
+// App
 const app = express();
 
 // Routers
 app.use(apiRoutes);
 
+// Start app listening
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App listening at: localhost:${PORT}`);
