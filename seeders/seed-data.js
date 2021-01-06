b39ccbce0679c49857dd75c09b6ef25ea6128a37
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-06 17:44:36 -0500

    Add seed data

---
+++ seeders/seed-data.js
@@ -0,0 +1,62 @@
+export const movies = [
+  {
+    imdbID: "1",
+    title: "Title 1",
+    year: "1900",
+    rated: "PG",
+    genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+    plot: "",
+    poster: "example.jpg",
+  },
+  {
+    imdbID: "2",
+    title: "Title 2",
+    year: "1900",
+    rated: "PG",
+    genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+    plot: "",
+    poster: "example.jpg",
+  },
+  {
+    imdbID: "3",
+    title: "Title 3",
+    year: "1900",
+    rated: "PG",
+    genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+    plot: "",
+    poster: "example.jpg",
+  },
+  {
+    imdbID: "4",
+    title: "Title 4",
+    year: "1900",
+    rated: "PG",
+    genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+    plot: "",
+    poster: "example.jpg",
+  },
+  {
+    imdbID: "5",
+    title: "Title 5",
+    year: "1900",
+    rated: "PG",
+    genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+    plot: "",
+    poster: "example.jpg",
+  },
+];
+
+export const lists = [
+  {
+    name: "Bob Ross",
+    movies: ["1", "2", "3", "4", "5"],
+  },
+  {
+    name: "Spongebob",
+    movies: ["1", "2", "3", "4", "5"],
+  },
+  {
+    name: "Patrick",
+    movies: ["1", "2", "3", "4", "5"],
+  },
+];
