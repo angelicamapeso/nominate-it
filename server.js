1ce66e7ee1534f9c1b776d999a1e038bed6a7094
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-06 15:34:16 -0500

    Add mongodb connection, server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import apiRoutes from "./routes/api-routes.js";
+import mongoose from "mongoose";
 
 // App
 const app = express();
@@ -9,6 +10,18 @@ app.use(apiRoutes);
 
 // Start app listening
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`App listening at: localhost:${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/top5db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening at: localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
