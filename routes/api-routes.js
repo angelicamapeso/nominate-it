9204f1f83b1227cabb2a92e14c097c9b597c9ce5
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-06 22:24:46 -0500

    Add post route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,4 +8,24 @@ router.get("/", async (req, res) => {
   res.send(result);
 });
 
+router.post("/", async (req, res) => {
+  // To ensure unique movie data
+  const bulkMovies = req.body.movies.map(movie => ({
+    updateOne: {
+      filter: { imdbID: movie.imdbID },
+      update: movie,
+      upsert: true,
+    },
+  }));
+  const bulkMovieResult = await Movie.bulkWrite(bulkMovies);
+  // Stripping movie info to insert as list
+  const movieIDs = req.body.movies.map(movie => movie.imdbID);
+  const movieList = { ...req.body, movies: movieIDs };
+  const movieListResult = await MovieList.create(movieList);
+  await movieListResult.populate("movie_data").execPopulate();
+  res.send({
+    data: { movieList: movieListResult, bulkMovieInsert: bulkMovieResult },
+  });
+});
+
 export default router;
