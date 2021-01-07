fee9ac210f50c3cb423806f0c9c61a9350440029
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-06 20:58:16 -0500

    Add get route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { MovieList } from "../models/index.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("It's ALIVE!!");
+router.get("/", async (req, res) => {
+  const result = await MovieList.find({}).populate("movie_data");
+  res.send(result);
 });
 
 export default router;
