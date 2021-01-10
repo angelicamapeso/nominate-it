b87377364c36ad83bfeec90e7a8be9b94c2f60bd
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-09 21:27:31 -0500

    Fix button classes

--- client/src/components/SearchCard/index.js
+++ client/src/components/SearchCard/index.js
@@ -12,10 +12,7 @@ function SearchCard() {
       />
       <div className="card-body p-0 d-flex flex-column justify-content-between">
         <p className="h5 p-3">Me And You And Everyone We Know (1978)</p>
-        <Button
-          className="w-100 align-self-end stretched-link py-2 card-button"
-          theme="grey"
-        >
+        <Button className="stretched-link py-2 card-button" theme="grey">
           <i className="fas fa-plus-circle mr-2"></i>Nominate
         </Button>
       </div>
