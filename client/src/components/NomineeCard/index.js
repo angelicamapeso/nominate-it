c38858ee2e904ac191bc3eb521a5e06beb5cb208
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-10 17:12:45 -0500

    Put image inside of card

--- client/src/components/NomineeCard/index.js
+++ client/src/components/NomineeCard/index.js
@@ -5,12 +5,12 @@ import "./style.css";
 function NomineeCard() {
   return (
     <li className="d-flex flex-row mb-3">
-      <img
-        className="card-image nominee-image border"
-        alt="Full Metal Jacket"
-        src="https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-      />
-      <div className="card flex-grow-1 nominee-card">
+      <div className="d-flex flex-row card flex-grow-1 nominee-card">
+        <img
+          className="card-image nominee-image border-right"
+          alt="Full Metal Jacket"
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+        />
         <div className="card-body p-0 d-flex flex-row justify-content-between">
           <p className="h6 p-3">Me and You and Everyone We Know (1978)</p>
           <Button theme="dark" className="remove-button stretched-link">
