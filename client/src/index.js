745112a06ae332b2825f19866a46a29e0625f460
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-10 16:53:37 -0500

    Import index styles
    sets height of body and html, and sets default color

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,6 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
