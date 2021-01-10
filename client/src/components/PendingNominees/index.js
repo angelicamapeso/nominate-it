980b6072c1528dad258de9e4da81aba86cab5c7d
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-10 11:28:05 -0500

    Add responsiveness to nomination list button

--- client/src/components/PendingNominees/index.js
+++ client/src/components/PendingNominees/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import Button from "../../components/Button";
 import "./style.css";
 
 function PendingNominees() {
+  const isLarge = useMediaQuery({ query: "(min-width: 992px)" });
+
+  const editButton = !isLarge ? (
+    <Button
+      theme="dark"
+      id="pending-edit"
+      type="button"
+      data-toggle="collapse"
+      data-target="#nomineeList"
+      aria-expanded="false"
+      aria-controls="nomineeList"
+    >
+      <i className="fas fa-pen"></i>
+    </Button>
+  ) : (
+    <></>
+  );
+
   return (
     <section className="d-flex flex-column" id="nominee-section">
-      <div className="d-flex flex-row order-2 order-lg-0">
+      <div className="d-flex flex-row">
         <div
           className="d-flex flex-row justify-content-between flex-grow-1"
           id="pending-title"
@@ -13,23 +32,13 @@ function PendingNominees() {
           <h2>
             <span id="pending-count">5</span> of 5 Nominations
           </h2>
-          <Button
-            theme="dark"
-            id="pending-edit"
-            type="button"
-            data-toggle="collapse"
-            data-target="#nomineeList"
-            aria-expanded="false"
-            aria-controls="nomineeList"
-          >
-            <i className="fas fa-pen"></i>
-          </Button>
         </div>
+        {editButton}
         <Button theme="light">
           <i className="fas fa-paper-plane"></i>
         </Button>
       </div>
-      <div className="collapse order-0 order-lg-1" id="nomineeList">
+      <div className={"collapse" + (isLarge ? "show" : "")} id="nomineeList">
         <ul>
           <li>List of items</li>
           <li>List of items</li>
