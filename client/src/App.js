b3ea5277a3a3bc4e72d968d1ee6b062c2a44f6b5
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-09 19:44:06 -0500

    Add nominees page to router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import Nominees from "./pages/Nominees";
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/nominees" component={Nominees} />
       </Switch>
     </Router>
   );
