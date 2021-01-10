9069f2b40553e08e74300c00af8abc1537cce73f
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-09 20:34:41 -0500

    Move nav into container

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,20 +4,24 @@ import Button from "../Button";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Nominate it!
-      </Link>
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link" to="/nominees">
-            <Button theme="dark">
-              <i className="fas fa-medal mr-2"></i>Nominees
-            </Button>
+    <section className="bg-light">
+      <div className="container">
+        <nav className="navbar navbar-light px-0">
+          <Link className="navbar-brand" to="/">
+            Nominate it!
           </Link>
-        </li>
-      </ul>
-    </nav>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/nominees">
+                <Button theme="dark">
+                  <i className="fas fa-medal mr-2"></i>Nominees
+                </Button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </section>
   );
 }
 
