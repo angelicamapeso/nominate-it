88f487555ff6af3157b8b7a6d1124661d528f372
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-10 16:54:37 -0500

    Add nominee card component

--- client/src/components/NomineeList/index.js
+++ client/src/components/NomineeList/index.js
@@ -1,25 +1,15 @@
 import React from "react";
-import Button from "../Button";
+import NomineeCard from "../NomineeCard";
 import "./style.css";
 
 function NomineeList() {
   return (
-    <ol id="nominees" className="px-5 px-lg-0 py-3">
-      <li className="d-flex flex-row mb-3">
-        <img
-          className="card-image nominee-image border"
-          alt="Full Metal Jacket"
-          src="https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-        />
-        <div className="card flex-grow-1 nominee-card">
-          <div className="card-body p-0 d-flex flex-row justify-content-between">
-            <p className="h6 p-3">Me and You and Everyone We Know (1978)</p>
-            <Button theme="dark" className="remove-button stretched-link">
-              <i className="fas fa-times-circle"></i>
-            </Button>
-          </div>
-        </div>
-      </li>
+    <ol className="px-5 px-lg-0 py-3 nominees">
+      <NomineeCard />
+      <NomineeCard />
+      <NomineeCard />
+      <NomineeCard />
+      <NomineeCard />
     </ol>
   );
 }
