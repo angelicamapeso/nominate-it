683d2c53c36ea85f6b3f94b85bce9e3c6f88dc0c
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 18:26:57 -0500

    Display first nomination list

--- client/src/pages/Nominees/index.js
+++ client/src/pages/Nominees/index.js
@@ -20,6 +20,25 @@ function Nominees() {
           <h1>Nominees</h1>
         </div>
       </section>
+      <div className="container-fluid px-lg-5 px-md-3">
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <NomineeList className="pl-0 mb-0 d-flex flex-row flex-wrap">
+                  {nominees.length > 0 ? (
+                    nominees[0].movies.map(nominee => (
+                      <NomineeCard movie={nominee} />
+                    ))
+                  ) : (
+                    <></>
+                  )}
+                </NomineeList>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
