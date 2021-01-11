0965427a0d204c4eb6d865b851cfb665f04e5be5
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-10 19:20:18 -0500

    Move nominee toggle into component

--- client/src/components/PendingNominees/index.js
+++ client/src/components/PendingNominees/index.js
@@ -1,51 +1,18 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
 import NomineeList from "../NomineeList";
-import Button from "../../components/Button";
+import NomineeToggle from "../NomineeToggle";
 import "./style.css";
 
 function PendingNominees() {
   const isLarge = useMediaQuery({ query: "(min-width: 992px)" });
-
-  const title = !isLarge ? (
-    <Button
-      className="flex-grow-1"
-      id="pending-title"
-      theme="dark"
-      type="button"
-      data-toggle="collapse"
-      data-target="#nomineeList"
-      aria-expanded="false"
-      aria-controls="nomineeList"
-    >
-      <p className="h6 d-flex flex-row justify-content-between align-items-center mb-0">
-        <span>
-          <span id="pending-count">5</span> of 5 Nominations
-        </span>
-        <i className="fas fa-pen"></i>
-      </p>
-    </Button>
-  ) : (
-    <div
-      className="d-flex flex-row justify-content-between align-items-center flex-grow-1"
-      id="pending-title"
-    >
-      <p className="h4 mb-0">
-        <span id="pending-count">5</span> of 5 Nominations
-      </p>
-    </div>
-  );
-
   return (
     <section
       className="d-flex flex-column justify-content-end"
       id="nominee-section"
     >
       <div className="d-flex flex-row">
-        {title}
-        <Button theme="light" className="p-3" id="send-nominations">
-          <i className="fas fa-paper-plane"></i>
-        </Button>
+        <NomineeToggle target="nomineeList" />
       </div>
       <div className={"collapse " + (isLarge ? "show" : "")} id="nomineeList">
         <NomineeList />
