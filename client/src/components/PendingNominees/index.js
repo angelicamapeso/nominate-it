7280f3d859dcdffe96c18d7bb5d3e660c05f3b39
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 00:16:53 -0500

    Add nominee count to pending nominees

--- client/src/components/PendingNominees/index.js
+++ client/src/components/PendingNominees/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import { useMediaQuery } from "react-responsive";
 import NomineeList from "../NomineeList";
 import NomineeToggle from "../NomineeToggle";
+import { usePending } from "../../utils/PendingContext";
 import "./style.css";
 
 function PendingNominees() {
   const isLarge = useMediaQuery({ query: "(min-width: 992px)" });
+  const { pending } = usePending();
   return (
     <section
       className="d-flex flex-column justify-content-end"
       id="nominee-section"
     >
       <div className="d-flex flex-row">
-        <NomineeToggle target="nomineeList" />
+        <NomineeToggle target="nomineeList" count={pending.length} />
       </div>
       <div className={"collapse " + (isLarge ? "show" : "")} id="nomineeList">
         <NomineeList />
