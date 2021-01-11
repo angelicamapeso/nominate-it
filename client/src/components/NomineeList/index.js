831a95252552646a36bbc53ae4a7da2d47e3ee47
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 00:38:44 -0500

    Display pending nominees

--- client/src/components/NomineeList/index.js
+++ client/src/components/NomineeList/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import NomineeCard from "../NomineeCard";
+import { usePending } from "../../utils/PendingContext";
 import "./style.css";
 
 function NomineeList() {
+  const { pending, removePending } = usePending();
   return (
     <ol className="px-5 px-lg-0 py-3 nominees">
-      <NomineeCard pending={true} />
-      <NomineeCard pending={true} />
-      <NomineeCard pending={true} />
-      <NomineeCard pending={true} />
-      <NomineeCard pending={true} />
+      {pending.map(movie => (
+        <NomineeCard
+          key={movie.imdbID}
+          movie={movie}
+          removePending={() => removePending(movie.imdbID)}
+        />
+      ))}
     </ol>
   );
 }
