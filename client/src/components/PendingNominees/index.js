02f92f56b02d3f74257a36c32529326fc466ee3e
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 14:55:15 -0500

    Add dismissible error message to nomineeList

--- client/src/components/PendingNominees/index.js
+++ client/src/components/PendingNominees/index.js
@@ -2,17 +2,27 @@ import React from "react";
 import { useMediaQuery } from "react-responsive";
 import NomineeList from "../NomineeList";
 import NomineeToggle from "../NomineeToggle";
+import Error from "../Error";
 import { usePending } from "../../utils/PendingContext";
 import "./style.css";
 
 function PendingNominees() {
   const isLarge = useMediaQuery({ query: "(min-width: 992px)" });
-  const { pending } = usePending();
+  const { errMessage, pending, setErrMessage } = usePending();
+
+  const error = errMessage ? (
+    <Error dismissible={true} onClick={() => setErrMessage("")}>
+      {errMessage}
+    </Error>
+  ) : (
+    <></>
+  );
   return (
     <section
       className="d-flex flex-column justify-content-end"
       id="nominee-section"
     >
+      {error}
       <div className="d-flex flex-row">
         <NomineeToggle target="nomineeList" count={pending.length} />
       </div>
