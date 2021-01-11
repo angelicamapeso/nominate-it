dfd684b853f0d9274500bdf33172c263910d28e7
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 18:24:28 -0500

    Display nominees title

--- client/src/pages/Nominees/index.js
+++ client/src/pages/Nominees/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import NomineeList from "../../components/NomineeList";
+import NomineeCard from "../../components/NomineeCard";
 import { getNominees } from "../../utils/API";
 
 function Nominees() {
@@ -10,7 +12,16 @@ function Nominees() {
       setNominees(response);
     });
   }, []);
-  return <h2>Nominee page</h2>;
+
+  return (
+    <>
+      <section className="bg-light pt-3 pb-2">
+        <div className="container-fluid px-lg-5 px-md-3">
+          <h1>Nominees</h1>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Nominees;
