e3150181b10a1aa590ebcf96836b5a00e3235279
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 18:23:56 -0500

    Add state to nominee page, getNominees on load

--- client/src/pages/Nominees/index.js
+++ client/src/pages/Nominees/index.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getNominees } from "../../utils/API";
 
 function Nominees() {
+  const [nominees, setNominees] = useState([]);
+
+  useEffect(() => {
+    getNominees().then(response => {
+      console.log(response);
+      setNominees(response);
+    });
+  }, []);
   return <h2>Nominee page</h2>;
 }
 
