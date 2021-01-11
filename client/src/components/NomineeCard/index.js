c7a94bfc0db48206fbf29be41926c2a0c73a7fff
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 00:38:19 -0500

    Add props to nomineecard

--- client/src/components/NomineeCard/index.js
+++ client/src/components/NomineeCard/index.js
@@ -9,12 +9,22 @@ function NomineeCard(props) {
         <img
           className="card-image nominee-image border-right"
           alt="Full Metal Jacket"
-          src="https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+          src={
+            props.movie.poster !== "N/A"
+              ? props.movie.poster
+              : "/top-5-empty.png"
+          }
         />
         <div className="card-body p-0 d-flex flex-row justify-content-between">
-          <p className="h6 p-3">Me and You and Everyone We Know (1978)</p>
-          {props.pending ? (
-            <Button theme="dark" className="remove-button stretched-link">
+          <p className="h6 p-3">
+            {props.movie.title} ({props.movie.year})
+          </p>
+          {props.movie.pending ? (
+            <Button
+              theme="dark"
+              className="remove-button stretched-link"
+              onClick={props.removePending}
+            >
               <i className="fas fa-times-circle"></i>
             </Button>
           ) : (
