c3e69daad746d3d89fd2a147714e3cac8ca940bd
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 15:39:21 -0500

    Add func to format nominee list for sending

--- client/src/utils/formatter.js
+++ client/src/utils/formatter.js
@@ -6,3 +6,15 @@ export function formatOMDBMovie(movie) {
     poster: movie.Poster,
   };
 }
+
+// need to make a deep copy of nominees
+// since removing the pending property
+export function formatToSend(nominees) {
+  const toSend = [];
+  for (const nominee of nominees) {
+    const toFormat = { ...nominee };
+    delete toFormat.pending;
+    toSend.push(toFormat);
+  }
+  return toSend;
+}
