d81793d2c4ef285c3b4a55d7c916b651ca5a6c1d
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 14:36:05 -0500

    Add dismissable option to alert

--- client/src/components/Error/index.js
+++ client/src/components/Error/index.js
@@ -2,8 +2,26 @@ import React from "react";
 
 function Error(props) {
   return (
-    <div className="alert alert-danger" role="alert">
+    <div
+      className={
+        "alert alert-danger " +
+        (props.dismissible ? `alert-dismissible fade show` : "")
+      }
+      role="alert"
+    >
       {props.children}
+      {props.dismissible ? (
+        <button
+          type="button"
+          class="close"
+          data-dismiss="alert"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
