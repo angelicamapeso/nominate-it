bc9689d94d659c732858957612c2007036f2bb4c
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 19:00:22 -0500

    Adjust spacing in layout

--- client/src/pages/Nominees/index.js
+++ client/src/pages/Nominees/index.js
@@ -15,17 +15,17 @@ function Nominees() {
 
   return (
     <>
-      <section className="bg-light pt-3 pb-2">
+      <section className="bg-light pt-3 pb-2 mb-4">
         <div className="container-fluid px-lg-5 px-md-3">
           <h1>Nominees</h1>
         </div>
       </section>
       <div className="container-fluid px-lg-5 px-md-3">
-        <div className="row">
-          <div className="col">
+        <div className="row mt-3">
+          <div className="col-12 col-md-6 col-xl-4">
             <div className="card">
               <div className="card-body">
-                <NomineeList className="pl-0 mb-0 d-flex flex-row flex-wrap">
+                <NomineeList className="pl-0 mb-0">
                   {nominees.length > 0 ? (
                     nominees[0].movies.map(nominee => (
                       <NomineeCard movie={nominee} />
