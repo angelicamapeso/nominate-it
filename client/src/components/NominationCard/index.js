f4d42aad9d045be64deb8b7fa965687cf4ea5637
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-12 17:23:31 -0500

    Move remove button outside card

--- client/src/components/NominationCard/index.js
+++ client/src/components/NominationCard/index.js
@@ -4,31 +4,35 @@ import "./style.css";
 
 function NominationCard(props) {
   return (
-    <div className="d-flex flex-row card flex-grow-1 nomination-card">
-      <img
-        className="card-image nomination-image border-right"
-        alt="Full Metal Jacket"
-        src={
-          props.movie.poster !== "N/A" ? props.movie.poster : "/top-5-empty.png"
-        }
-      />
-      <div className="card-body p-0 d-flex flex-row justify-content-between">
-        <p className="h6 p-3">
-          {props.movie.title} ({props.movie.year})
-        </p>
-        {props.movie.pending ? (
-          <Button
-            theme="dark"
-            className="remove-button"
-            onClick={props.removePending}
-          >
-            <i className="fas fa-times-circle"></i>
-          </Button>
-        ) : (
-          ""
-        )}
+    <>
+      <div className="d-flex flex-row card flex-grow-1 nomination-card">
+        <img
+          className="card-image nomination-image border-right"
+          alt="Full Metal Jacket"
+          src={
+            props.movie.poster !== "N/A"
+              ? props.movie.poster
+              : "/top-5-empty.png"
+          }
+        />
+        <div className="card-body p-0 d-flex flex-row justify-content-between">
+          <p className="h6 p-3">
+            {props.movie.title} ({props.movie.year})
+          </p>
+        </div>
       </div>
-    </div>
+      {props.movie.pending ? (
+        <Button
+          theme="dark"
+          className="remove-button"
+          onClick={props.removePending}
+        >
+          <i className="fas fa-times-circle"></i>
+        </Button>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
 
