3da32ad257c216ce469fe9c1900cd5b7089a0dac
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-11 20:43:08 -0500

    Update seeder to match omdb N/A poster

--- seeders/seed-data.js
+++ seeders/seed-data.js
@@ -3,31 +3,31 @@ export const movies = [
     imdbID: "1",
     title: "Title 1",
     year: "1900",
-    poster: "example.jpg",
+    poster: "N/A",
   },
   {
     imdbID: "2",
     title: "Title 2",
     year: "1900",
-    poster: "example.jpg",
+    poster: "N/A",
   },
   {
     imdbID: "3",
     title: "Title 3",
     year: "1900",
-    poster: "example.jpg",
+    poster: "N/A",
   },
   {
     imdbID: "4",
     title: "Title 4",
     year: "1900",
-    poster: "example.jpg",
+    poster: "N/A",
   },
   {
     imdbID: "5",
     title: "Title 5",
     year: "1900",
-    poster: "example.jpg",
+    poster: "N/A",
   },
 ];
 
